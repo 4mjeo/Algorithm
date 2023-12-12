aedafb27808ec81cbb34812ab7e82196b2b2bbd3
4mjeo <4mj.eo@dsm.hs.kr>
2023-12-13 08:35:48 +0900

    update :: 1343

--- 실버/1343.c
+++ 실버/1343.c
@@ -4,20 +4,21 @@ int main(){
     char c[51];
     int cnt=0;
     scanf("%s",c);
-    for(int i=0;i<strlen(c);i++){
-        if((strlen(c)-3>=0) && c[i]=='X' && c[i+1]=='X' && c[i+2]=='X' && c[i+3]=='X'){
+  int len = strlen(c);
+    for(int i=0;i<len;i++){
+        if((len-3>=0) && c[i]=='X' && c[i+1]=='X' && c[i+2]=='X' && c[i+3]=='X'){
             c[i]='A';
             c[i+1]='A';
             c[i+2]='A';
             c[i+3]='A';
         }
-        else if((strlen(c)-1>=0) && c[i]=='X' && c[i+1]=='X'){
+        else if((len-1>=0) && c[i]=='X' && c[i+1]=='X'){
             c[i]='B';
             c[i+1]='B';
         }
         continue;
     }
-    for(int i=0;i<strlen(c);i++){
+    for(int i=0;i<len;i++){
         if(c[i]=='X'){
             cnt=1;
             break;
@@ -25,7 +26,7 @@ int main(){
     }
     if(cnt==1) printf("-1");
     else{
-        for(int i=0;i<strlen(c);i++){
+        for(int i=0;i<len;i++){
             printf("%c",c[i]);
         }
     }
