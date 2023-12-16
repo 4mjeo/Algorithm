f8d235043764bea0d326adf990a79af10954f7cd
4mjeo <4mj.eo@dsm.hs.kr>
2023-12-17 01:33:54 +0900

    update :: 30923

--- 실버/30923.c
+++ 실버/30923.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    long long int n,a,b,first;
+    int n,a,b,first;
     scanf("%lld",&n);
-    int r = n*2;
+    long long int r = n*2;
     scanf("%lld",&a);
     first = a;
     r += a*2;
     b = a;
     for(int i=1;i<n;i++){
-        scanf("%lld",&a);
+        scanf("%d",&a);
         r += a*2;
         r += abs(a-b);
         b = a;
