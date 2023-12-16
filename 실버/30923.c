59dd0a179d458ee02973364fced88a337815f8fd
4mjeo <4mj.eo@dsm.hs.kr>
2023-12-17 00:58:31 +0900

    30923

---
+++ 실버/30923.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    long long int n;
+    scanf("%lld",&n);
+    long long int a[n];
+    int r=n*2;
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        r += a[i]*2;
+        if(i>0){
+            r += abs(a[i]-a[i-1]);
+        }
+    }
+    r += a[0]+a[n-1];
+    printf("%lld\n",r);
+    return 0;
+}
