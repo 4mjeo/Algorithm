ea533cf6498479abba5e7869514a2c96592d37fe
4mjeo <4mj.eo@dsm.hs.kr>
2023-12-18 11:47:16 +0900

    1100

---
+++ 브론즈/1100.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char a[8][9];
+    int cnt=0;
+    for(int i=0;i<8;i++){
+        scanf("%s",a[i]);
+    }
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            if((i+j)%2==0 && c[i][j]=='F') cnt++;
+        }
+    }
+    printf("%d",cnt);
+    return 0;
+}
