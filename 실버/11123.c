fcc5f74a446d899601b31967ef1f4e6201a9b27d
4mjeo <4mj.eo@dsm.hs.kr>
2023-12-30 23:13:36 +0900

    update :: 11123

--- 실버/11123.c
+++ 실버/11123.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-int h, w, vis[101][101];
+int h, w;
 char graph[101][101];
 void dfs(int x, int y) {
-    if(x<0 || y<0 || x>=h || y>=w || vis[x][y] || graph[x][y] != '#') return;
-    vis[x][y] = 1;
-    dfs(x, y + 1);
-    dfs(x, y - 1);
-    dfs(x + 1, y);
-    dfs(x - 1, y);
+    if(x<0 || y<0 || x>=h || y>=w || graph[x][y] != '#') return;
+    graph[x][y] = '.';
+    dfs(x, y+1);
+    dfs(x, y-1);
+    dfs(x+1, y);
+    dfs(x-1, y);
 }
 int main() {
     int t;
     scanf("%d", &t);
-    for (int i=0;i<t;i++) {
-        scanf("%d %d", &h,&w);
-        for(int j=0;j<h;j++) {
-            for(int k=0;k<w;k++) {
-                graph[j][k] = 0;
-                vis[j][k] = 0;
-            }
+    while(t--) {
+        scanf("%d %d", &h, &w);
+        for(int j=0; j<h; j++) {
             scanf("%s", graph[j]);
         }
         int cnt = 0;
-        for (int j=0;j<h;j++) {
-            for (int k=0;k<w;k++) {
-                if (!vis[j][k] && graph[j][k] == '#') {
+        for (int j=0; j<h; j++) {
+            for (int k=0; k<w; k++) {
+                if (graph[j][k] == '#') {
                     dfs(j, k);
                     cnt++;
                 }
