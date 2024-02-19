5965dad44e768f060917e18448bd4467aa16aeab
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-20 01:26:19 +0900

    날먹 :: 5717

---
+++ 브론즈/5717.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    while(1) {
+        int a,b;
+        scanf("%d %d",&a,&b);
+        if(a==0 && b==0) break;
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
