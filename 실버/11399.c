f09d11f43ca73df2f4b687903730a33852e03359
4mjeo <4mj.eo@dsm.hs.kr>
2024-05-01 17:22:41 +0900

    update :: 11399

--- 실버/11399.c
+++ 실버/11399.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-void sort(int arr[], int n){
-    int i,j,temp;
-    for(i=0;i<n-1;i++){
-        for(j=0;j<n-i-1;j++){
-            if(arr[j] > arr[j+1]){
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+  int num1 = *(int *)a;
+  int num2 = *(int *)b;
+  if(num1 < num2) return -1;
+  if(num1 > num2) return 1;
+  return 0;
 }
 int main(){
     int a,r=0,time[1001];
@@ -17,7 +13,7 @@ int main(){
     for(int i=0;i<a;i++){
         scanf("%d",&time[i]);
     }
-    sort(time,a);
+    qsort(time, a, sizeof(int), compare);
     for(int i=0;i<a;i++){
         for(int j=0;j<=i;j++){
             r += time[j];
