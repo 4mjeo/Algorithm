6a56da7d331ba0e5a616cf6dea7e48ea367c9221
4mjeo <4mj.eo@dsm.hs.kr>
2024-05-29 23:39:40 +0900

    2501

---
+++ 브론즈/2501.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int n,k,cnt=0;
+  scanf("%d %d",&n,&k);
+  for(int i=1;i<=n;i++) {
+    if(n%i == 0) {
+      cnt++;
+      if(cnt == k) {
+        printf("%d",i);
+        return 0;
+      }
+    }
+  } 
+  printf("0");
+  return 0;
+}
